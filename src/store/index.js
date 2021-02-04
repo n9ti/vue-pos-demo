d97e9164ede8d2b05e7e39be7d02dfe547c4d506
Nati Namvong <nati@natis-mac-mini.lan>
2021-02-04 23:18:28 +0700

    finish pos demo

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,150 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: [],
+    cart: []
+  },
+  mutations: {
+    addToCart(state, newProduct) {
+      let index = state.cart.findIndex(product => product.id === newProduct.id);
+
+      if (index >= 0) {
+        state.cart[index].amount++;
+      } else {
+        state.cart.push({ ...newProduct, amount: 1 });
+      }
+    },
+    setProduct(state, products) {
+      state.products = products;
+    }
+  },
+  getters: {
+    net(state) {
+      return state.cart.reduce(
+        (sumPrice, product) => sumPrice + product.price * product.amount,
+        0
+      );
+    }
+  },
+  actions: {
+    getProduct({ commit }) {
+      let products = [
+        {
+          id: "001",
+          name: "iPad Pro",
+          price: 30900,
+          storage: 32,
+          type: "iPad",
+          size: "12.9"
+        },
+        {
+          id: "002",
+          name: "iPad Pro",
+          price: 34900,
+          storage: 128,
+          type: "iPad",
+          size: "12.9"
+        },
+        {
+          id: "003",
+          name: "iPad Pro",
+          price: 38900,
+          storage: 256,
+          type: "iPad",
+          size: "12.9"
+        },
+        {
+          id: "004",
+          name: "iPad Pro",
+          price: 22900,
+          storage: 32,
+          type: "iPad",
+          size: "9.7"
+        },
+        {
+          id: "005",
+          name: "iPad Pro",
+          price: 26900,
+          storage: 128,
+          type: "iPad",
+          size: "9.7"
+        },
+        {
+          id: "006",
+          name: "iPad Pro",
+          price: 30900,
+          storage: 256,
+          type: "iPad",
+          size: "9.7"
+        },
+        {
+          id: "007",
+          name: "iPad Air 2",
+          price: 14400,
+          storage: 32,
+          type: "iPad",
+          size: "9.7"
+        },
+        {
+          id: "008",
+          name: "iPad Air 2",
+          price: 18400,
+          storage: 128,
+          type: "iPad",
+          size: "9.7"
+        },
+        {
+          id: "009",
+          name: "iPad mini 4",
+          price: 14400,
+          storage: 32,
+          type: "iPad",
+          size: "7.9"
+        },
+        {
+          id: "010",
+          name: "iPad mini 4",
+          price: 18400,
+          storage: 128,
+          type: "iPad",
+          size: "7.9"
+        },
+        {
+          id: "011",
+          name: "iphone7",
+          price: 26000,
+          storage: 32,
+          type: "iPhone",
+          size: "4.7"
+        },
+        {
+          id: "012",
+          name: "iphone7",
+          price: 55000,
+          storage: 128,
+          type: "iPhone",
+          size: "4.7"
+        },
+        {
+          id: "013",
+          name: "iphone7Plus",
+          price: 49000,
+          storage: 32,
+          type: "iPhone",
+          size: "5.5"
+        },
+        {
+          id: "014",
+          name: "iphone7Plus",
+          price: 59000,
+          storage: 128,
+          type: "iPhone",
+          size: "5.5"
+        }
+      ];
+      commit("setProduct", products);
+    }
+  },
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Products</h1>
+    <div v-for="product in products" :key="product.id">
+      {{ product.name }} {{ product.price }}
+      <button @click="addToCart(product)">Add to cart</button>
+    </div>
+    <br />
+    <hr />
+    <br />
+    <h1>Cart</h1>
+    <div v-for="product in cart" :key="product.id">
+      {{ product.name }} {{ product.price }} x{{ product.amount }}
+      <button @click="addToCart(product)">Add to cart</button>
+    </div>
+    <h1>รวม {{ net }}</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mapState, mapMutations, mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  mounted() {
+    this.getProduct();
+  },
+  data() {
+    return {
+      v: 0
+    };
+  },
+  computed: {
+    ...mapState(["products", "cart"]),
+    ...mapGetters(["net"])
+  },
+  methods: {
+    ...mapMutations(["addToCart"]),
+    ...mapActions(["getProduct"])
   }
 };
 </script>
